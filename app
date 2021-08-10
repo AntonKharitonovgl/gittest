5f29439e79604084ff491f74cc4a6407d57bb642
Anton Kharitono <stimulrus@gmail.com>
2021-08-10 15:50:23 +0300

    Поправка ошибок на ветке dev

--- app
+++ app
@@ -1,11 +1,14 @@
 <?php
+use \App\FirstExample;
+use \App\SecondExample;
+use \App\ThirdExample;
 
 require_once  __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 $app = new \Symfony\Component\Console\Application('demo application');
 
-$app->add(new \App\firstexample());
-$app->add(new \App\secondexample());
-$app->add(new \App\thirdexample());
+$app->add(new FirstExample());
+$app->add(new SecondExample());
+$app->add(new ThirdExample());
 
 $app->run();
--- src/ThirdExample.php
+++ src/ThirdExample.php
@@ -22,13 +22,17 @@ class ThirdExample extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $helper = $this->getHelper('question');
+        
         $question = new Question('Please enter the name', 'AcmeDemoBundle');
         $name = $helper->ask($input, $output, $question);
+        
         $question = new Question('Please enter the age', 'AcmeDemoBundle');
         $age = $helper->ask($input, $output, $question);
+        
         $question = new ChoiceQuestion('Please select your sex (defaults to male)', ['male', 'female'], 0);
         $question->setErrorMessage('Sex %s is invalid.');
         $sex = $helper->ask($input, $output, $question);
+        
         $output->writeln('Привет '. $name . ', ваш возраст: ' . $age . ', ваш пол: ' . $sex);
         return Command::SUCCESS;
     }
